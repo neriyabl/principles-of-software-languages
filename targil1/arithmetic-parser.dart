09bb59a7b15d4db071b1af133b69ebf8c0e90ca7
Neriya Bar-Lev <neriyabl555@gmail.com>
2019-03-19 20:49:14 +0200

    create add func

---
+++ targil1/arithmetic-parser.dart
@@ -0,0 +1,19 @@
+
+
+main(){
+  print(Add());
+}
+
+
+String Add() {
+  return '@0\n'
+         'M=M-1\n'
+         'A=M\n'
+         'D=M\n'
+         'A=A-1\n'
+         'A=M\n'
+         'D=D+A\n'
+         '@0\n'
+         'A=M-1\n'
+         'M=D';
+}
