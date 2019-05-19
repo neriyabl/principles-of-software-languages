a6cacdbe6c5bfdc64e6afb64417149462ab00189
Neriya Bar-Lev <neriyabl555@gmail.com>
2019-05-19 03:26:24 +0300

    initial targil 4

---
+++ targil4/tokenizing.dart
@@ -0,0 +1,10 @@
+class Tokenizer {
+  final _keyword = new RegExp(
+      r'^(class|constructor|function|method|field|static|var|int|char|boolean|void|true|false|null|this|let|do|if|else|while|return)$');
+
+  final _sympol = new RegExp(r"^([{}()\[\].,;+\-*/&|<>=~])$");
+
+  final _integerConstant = new RegExp(r'^([1-2]?[0-9]?[0-9]?[0-9]?[0-9]|3[0-1][0-9]{3}|32[0-6][0-9]{2}|327[0-5][0-9]|3276[0-7])$');
+
+  final _stringConstant = new RegExp(r'^"[^"]*"$');
+}
