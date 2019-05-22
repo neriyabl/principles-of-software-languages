d16f8fe4b27382e43fe5dc87cd7c63c959c033d3
netanel96 <45965819+netanel96@users.noreply.github.com>
2019-05-22 03:06:28 +0300

    עשיתי איזה משהו די ידני רק חסר לייבא את הספרייה XML ואז להשתמש בפונקציה PARSE שזה רק מאמתת שזה באמת XML תקין ואז אפשר ליצור את זה לתוך קובץ טקסט. לא הייתי בטוח איך להוסיף את הספרייה ואם כדאי לכתוב לקובץ טקסט רגיל.

--- targil4/tokenizing.dart
+++ targil4/tokenizing.dart
@@ -2,13 +2,15 @@ import 'dart:html';
 
 import 'Entities.dart';
 
+
 class Tokenizer {
   final _keyword = new RegExp(
       r'^(class|constructor|function|method|field|static|var|int|char|boolean|void|true|false|null|this|let|do|if|else|while|return)$');
 
   final _sympol = new RegExp(r"^([{}()\[\].,;+\-*/&|<>=~])$");
 
-  final _integerConstant = new RegExp(r'^([1-2]?[0-9]?[0-9]?[0-9]?[0-9]|3[0-1][0-9]{3}|32[0-6][0-9]{2}|327[0-5][0-9]|3276[0-7])$');
+  final _integerConstant = new RegExp(
+      r'^([1-2]?[0-9]?[0-9]?[0-9]?[0-9]|3[0-1][0-9]{3}|32[0-6][0-9]{2}|327[0-5][0-9]|3276[0-7])$');
 
   final _stringConstant = new RegExp(r'^"[^"]*"$');
 
@@ -17,12 +19,71 @@ class Tokenizer {
 
   Tokenizer(this.inputFile);
 
-  initialState () {
-    List<Token> tokenStream;
-    for(var line in inputFile){
-      for(var character in line){
+  void ExportFileToXML(List<Token> tokenStream,String JackFileName)
+  {
+    String startTok="<token>\n";
+    String resultedXML="";
+    String EndTok="</token>";
+
+    String startTokVal;
+    String tokenValue;
+    String EndTokVal;
+    for (int i=0;i<tokenStream.length;i++)
+    {
+      TokenType tokenType=tokenStream[i].type;
+      switch (tokenType.toString())
+      {
+        case 'keyword':
+          startTokVal="<keyword>";
+          tokenValue=tokenStream[i].value;
+          EndTokVal="</keyword>\n";
+          resultedXML = resultedXML + startTokVal + tokenValue + EndTokVal;
+          break;
+        case 'symbol':
+          startTokVal="<symbol>";
+          tokenValue=tokenStream[i].value;
+          EndTokVal="</symbol>\n";
+          resultedXML = resultedXML + startTokVal + tokenValue + EndTokVal;
+          break;
+        case 'integerConstant':
+          startTokVal="<integerConstant>";
+          tokenValue=tokenStream[i].value;
+          EndTokVal="</integerConstant>\n";
+          resultedXML = resultedXML + startTokVal + tokenValue + EndTokVal;
+          break;
+        case 'stringConstant':
+          startTokVal="<stringConstant>";
+          tokenValue=tokenStream[i].value;
+          EndTokVal="</stringConstant>\n";
+          resultedXML = resultedXML + startTokVal + tokenValue + EndTokVal;
+          break;
+        case 'identifier':
+          startTokVal="<identifier>";
+          tokenValue=tokenStream[i].value;
+          EndTokVal="</identifier>\n";
+          resultedXML = resultedXML + startTokVal + tokenValue + EndTokVal;
+          break;
+      }
     }
+    resultedXML = startTok + resultedXML +"\n"+ EndTok;
+/////////////////////to verify input as XML//////////////////
+    try {
+      //var document = xml.parse(bookshelfXml);
+    }
+    on Exception catch(e) {}
+
+////////////////////create text file and write xml//////////////////
+    //var XmlFile = new File(JackFileName + 'T.XML').openWrite();
+    //XmlFile.write(resultedXML);
   }
 
+
+
+  initialState () {
+    List<Token> tokenStream;
+    for (var line in inputFile) {
+      for (var character in line) {}
+    }
+  }
 
 }
