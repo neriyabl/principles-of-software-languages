51ebc68dbe5b113af07bdaabb23d02c108439684
Neriya Bar-Lev <neriyabl555@gmail.com>
2019-06-18 01:11:17 +0300

    fix tokenizer keyword match bug

--- targil4/tokenizing.dart
+++ targil4/tokenizing.dart
@@ -6,7 +6,7 @@ class Tokenizer {
   static final _commentLine = new RegExp(r'//[^\n]*');
   static final _commentMultyLint = new RegExp(r'/\*((?!\*/)[\s\S])*\*/');
   static final _keyword = new RegExp(
-    r'^(class|constructor|function|method|field|static|var|int|char|boolean|void|true|false|null|this|let|do|if|else|while|return)$');
+    r'^(class|constructor|function|method|field|static|var|int|char|boolean|void|true|false|null|this|let|do|if|else|while|return)[^\w]$');
   static final _symbol = new RegExp(r"^([{}()\[\].,;+\-*/&|<>=~])$");
   static final _integerConstant = new RegExp(
     r'^([1-2]?[0-9]?[0-9]?[0-9]?[0-9]|3[0-1][0-9]{3}|32[0-6][0-9]{2}|327[0-5][0-9]|3276[0-7])$');
